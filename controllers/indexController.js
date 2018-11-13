981e452371ca6731fffb185de82067ac9c2a894e
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2018-11-13 18:11:35 -0500

    Home Page
    Home Page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,32 @@
 "use strict";
 
+var Articles = require("../models/article");
+
+//GET: /
 function indexHome(request, response) {
+  /**
+   * Hacer una consulta de los ultimos 20 registros de articulos
+   * y enviarlos mediante socket.io para posteriormente renderizarlos
+   */
+  request.io.on("connection", socket => {
+    Articles.find({})
+      .sort({ date: -1 })
+      .exec((err, docs) => {
+        if (err) return console.error("Error: ", err);
+        socket.emit("searchText", false);
+        socket.emit("articles", docs);
+      });
+  });
+
+  /**
+   * Renderizar el template index.html y con el componente
+   * de search.html
+   */
   response.render("index", {
-    pageRoutes: ["layout/contenido-blog"],
+    pageRoutes: ["layout/search"],
     title: "InDeepLab | Blog de divulgación Cientifica",
-    scriptJS: []
+    scriptJS: ["/socket.io/socket.io.js", "/javascripts/search.js"],
+    session: request.session
   });
 }
 
